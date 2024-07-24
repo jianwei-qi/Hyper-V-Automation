49df07018f7fb7a4dbb4e2b303e1f0f668b59e12
jerryqi <630751892@qq.com>
2024-07-24 16:55:29 +0800

    Update $CurrentPath for each function in psm1

--- HVAModule.psm1
+++ HVAModule.psm1
@@ -2,7 +2,12 @@ Get-ChildItem -Path $PSScriptRoot -Filter '*.ps1' | ForEach-Object {
     $ScriptFile = $_.FullName
     $FunctionName = [System.IO.Path]::GetFileNameWithoutExtension($ScriptFile)
 
-    $FunctionBody = "$ScriptFile @Args"
+    $FunctionBody = @"
+`$CurrentPath = Get-Location
+Set-Location -Path $PSScriptRoot
+$ScriptFile @Args
+Set-Location -Path `$CurrentPath
+"@
 
     $scriptBlock = [scriptblock]::Create($FunctionBody)
     Set-Item -Path "function:$FunctionName" -Value $scriptBlock
